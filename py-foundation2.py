6aa6cfada19cf23f358b9b56e1ee8ab561e33537
Yushin <ogasawara01234@gmail.com>
2024-06-03 09:29:46 +0900

    Python基礎2条件分岐達成のため

---
+++ py-foundation2.py
@@ -0,0 +1,16 @@
+def main():
+    conditionalBranch()
+
+def conditionalBranch():
+    num = 10
+    if num > 10:
+	    print("numは10以上")
+    elif num == 10:
+	    print("numは10")
+    else:
+	    print("numは10以下")
+
+def repeationg:
+    
+if __name__ == "__main__":
+    main()
